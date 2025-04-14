0cb5bfd35ec3f47c5ba1f4a098bb5afb762cd4b7
Matthew Cane <matthew.cane@citizensadvice.org.uk>
2025-04-14 14:57:31 +0100

    Rework HPA

--- ca_cdk8s_constructs/hpa.py
+++ ca_cdk8s_constructs/hpa.py
@@ -5,10 +5,10 @@ from constructs import Construct
 
 def ca_hpa(
     scope: Construct,
-    max_replicas: int,
     target: kplus.IScalable,
-    cpu_utilization: Number = 80,
-    memory_utilization: Number = 80,
+    cpu_utilization_target: Number = 75,
+    memory_utilization_target: Number = 75,
+    max_replicas: int = 3,
     min_replicas: int = 1,
 ) -> kplus.HorizontalPodAutoscaler:
     """Create a HorizontalPodAutoscaler for a target.
@@ -19,18 +19,18 @@ def ca_hpa(
         scope: The scope of the construct.
         max_replicas: The maximum number of replicas.
         target: The target to autoscale.
-        cpu_utilization: The CPU utilization to target. Defaults to 80.
-        memory_utilization: The memory utilization to target. Defaults to 80.
+        cpu_utilization_target: The CPU utilization to target. Must be between 50 and 90. Defaults to 75.
+        memory_utilization_target: The memory utilization to target. Must be between 50 and 90. Defaults to 75.
         min_replicas: The minimum number of replicas. Defaults to 1.
 
     Returns:
         A HorizontalPodAutoscaler.
     """
     if (
-        cpu_utilization > 100
-        or cpu_utilization < 1
-        or memory_utilization > 100
-        or memory_utilization < 1
+        cpu_utilization_target > 90
+        or cpu_utilization_target < 50
+        or memory_utilization_target > 90
+        or memory_utilization_target < 50
     ):
         raise ValueError("Utilization values must be between 1 and 100")
 
@@ -41,17 +41,11 @@ def ca_hpa(
         min_replicas=min_replicas,
         target=target,
         metrics=[
-            kplus.Metric.container_memory(
-                container=container,
-                target=kplus.MetricTarget.average_utilization(memory_utilization),
-            )
-            for container in target.containers
-        ]
-        + [
-            kplus.Metric.container_cpu(
-                container=container,
-                target=kplus.MetricTarget.average_utilization(cpu_utilization),
-            )
-            for container in target.containers
+            kplus.Metric.resource_cpu(
+                target=kplus.MetricTarget.average_utilization(cpu_utilization_target)
+            ),
+            kplus.Metric.resource_memory(
+                target=kplus.MetricTarget.average_utilization(memory_utilization_target)
+            ),
         ],
     )
--- tests/test_hpa.py
+++ tests/test_hpa.py
@@ -1,33 +1,35 @@
+import pytest
 from ca_cdk8s_constructs.hpa import ca_hpa
 from cdk8s_plus_31 import Deployment
 from cdk8s import Chart
+from cdk8s_plus_31 import HorizontalPodAutoscaler
 
 
-def test_hpa_no_containers(chart_fixture: Chart):
+def test_hpa_default(chart_fixture: Chart):
     deployment = Deployment(chart_fixture, "test")
-    hpa = ca_hpa(chart_fixture, max_replicas=3, target=deployment)
+    hpa = ca_hpa(chart_fixture, target=deployment)
+    assert isinstance(hpa, HorizontalPodAutoscaler)
+    assert hpa.min_replicas == 1
     assert hpa.max_replicas == 3
-    assert len(hpa.metrics) == 0
 
 
-def test_hpa_with_containers(chart_fixture: Chart):
+def test_hpa_custom_values(chart_fixture: Chart):
     deployment = Deployment(chart_fixture, "test")
-    deployment.add_container(
-        image="nginx",
+    hpa = ca_hpa(
+        chart_fixture,
+        max_replicas=5,
+        min_replicas=2,
+        cpu_utilization_target=80,
+        memory_utilization_target=80,
+        target=deployment,
     )
-    hpa = ca_hpa(chart_fixture, max_replicas=3, target=deployment)
-    assert hpa.max_replicas == 3
-    assert len(hpa.metrics) == 2
+    assert hpa.max_replicas == 5
+    assert hpa.min_replicas == 2
 
 
-def test_hpa_with_multiple_containers(chart_fixture: Chart):
+def test_hpa_invalid_values(chart_fixture: Chart):
     deployment = Deployment(chart_fixture, "test")
-    deployment.add_container(
-        image="nginx",
-    )
-    deployment.add_container(
-        image="alpine",
-    )
-    hpa = ca_hpa(chart_fixture, max_replicas=3, target=deployment)
-    assert hpa.max_replicas == 3
-    assert len(hpa.metrics) == 4
+    with pytest.raises(ValueError):
+        ca_hpa(chart_fixture, target=deployment, cpu_utilization_target=100)
+    with pytest.raises(ValueError):
+        ca_hpa(chart_fixture, target=deployment, cpu_utilization_target=20)
