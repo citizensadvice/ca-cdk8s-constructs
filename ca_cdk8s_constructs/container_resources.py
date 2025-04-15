1f8dbfea81c0a017272dff38336c36688935d54c
Matthew Cane <matthew.cane@citizensadvice.org.uk>
2025-04-15 12:37:39 +0100

    Use Cpu object for arguments, standardise imports from cdk8s_plus

--- ca_cdk8s_constructs/container_resources.py
+++ ca_cdk8s_constructs/container_resources.py
@@ -1,30 +1,24 @@
 from cdk8s_plus_32 import ContainerResources, CpuResources, Cpu, MemoryResources
 from cdk8s import Size
-from numbers import Number
 
 
-def ca_container_resources(cpu: Number, memory: Number):
+def ca_container_resources(cpu: Cpu, memory: Size):
     """Create a ContainerResources object.
 
     It is not recommended to set cpu limit so this value is not set by this construct.
     The memory limit will be set equal to the memory request.
 
     Args:
-        cpu: The CPU requests in millicores (1000 = 1 CPU core).
-        memory: The memory request in mebibytes (1024 = 1 GiB).
+        cpu: The CPU request.
+        memory: The memory request and limit.
     """
-
-    if cpu < 100:
-        raise ValueError("CPU requests must be greater than or equal to 100 millicores")
-    if memory < 256:
-        raise ValueError("Memory requests must be greater than or equal to 256 mebibytes")
 
     return ContainerResources(
         cpu=CpuResources(
-            request=Cpu.millis(amount=cpu),
+            request=cpu,
         ),
         memory=MemoryResources(
-            limit=Size.mebibytes(memory),
-            request=Size.mebibytes(memory),
+            limit=memory,
+            request=memory,
         ),
     )
--- tests/test_container_resources.py
+++ tests/test_container_resources.py
@@ -1,18 +1,15 @@
 import pytest
 from ca_cdk8s_constructs.container_resources import ca_container_resources
-from cdk8s_plus_32 import Deployment
-from cdk8s import Chart
+from cdk8s_plus_32 import Deployment, Cpu
+from cdk8s import Chart, Size
 
 
 def test_container_resources(chart_fixture: Chart):
     deployment = Deployment(chart_fixture, "test")
-    container_resources = ca_container_resources(cpu=100, memory=512)
+    container_resources = ca_container_resources(
+        cpu=Cpu.millis(100), memory=Size.mebibytes(512)
+    )
     deployment.add_container(
         image="nginx",
         resources=container_resources,
     )
-
-
-def test_container_resources_error():
-    with pytest.raises(ValueError):
-        ca_container_resources(cpu=50, memory=125)
