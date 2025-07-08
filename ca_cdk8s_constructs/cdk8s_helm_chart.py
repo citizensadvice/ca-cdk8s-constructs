1aae59bc77a8ae8076071d4aec41557f34f63d01
Matthew Cane <matthew.cane@citizensadvice.org.uk>
2025-07-08 10:22:03 +0100

    Refactor helm hook application to use a resource protocol

--- ca_cdk8s_constructs/cdk8s_helm_chart.py
+++ ca_cdk8s_constructs/cdk8s_helm_chart.py
@@ -11,8 +11,9 @@ import yaml
 from aws_cdk import Duration
 from aws_cdk.aws_eks import HelmChart, ICluster
 from aws_cdk.aws_s3_assets import Asset
-from cdk8s_plus_32 import Job
 from constructs import Construct
+
+from .helpers import HasApiObjectMetadata
 
 
 class HelmChartAsset(Construct):
@@ -181,29 +182,31 @@ class HelmHookDeletePolicy(Enum):
 
 
 def apply_helm_hook(
-    job: Job,
+    resource: HasApiObjectMetadata,
     hook_types: Sequence[HelmHookType],
     hook_delete_policy: Sequence[HelmHookDeletePolicy] = [
         HelmHookDeletePolicy.BEFORE_HOOK_CREATION
     ],
-    weight: int = None,
+    weight: typing.Optional[int] = None,
 ) -> None:
-    """Apply helm hook annotations to a job.
+    """Apply helm hook annotations to a resource.
 
-    This applies the correct annotations to the job to ensure that the job is executed as
+    This applies the correct annotations to the resource to ensure that the resource is executed as
     a helm hook if the resource is deployed as a helm chart.
 
     Args:
-        job: The job to apply the helm hook to.
+        resource: The resource to apply the helm hook to. Must have a metadata attribute with add_annotation method.
         hook_types: The types of hook to apply.
         hook_delete_policy: The policy for deleting the hook resource after the hook has been executed.
         weight: The weight of the hook.
     """
-    job.metadata.add_annotation(
+    resource.metadata.add_annotation(
         "helm.sh/hook-delete-policy", ",".join([policy.value for policy in hook_delete_policy])
     )
 
-    job.metadata.add_annotation("helm.sh/hook", ",".join([hook.value for hook in hook_types]))
+    resource.metadata.add_annotation(
+        "helm.sh/hook", ",".join([hook.value for hook in hook_types])
+    )
 
     if weight:
-        job.metadata.add_annotation("helm.sh/hook-weight", str(weight))
+        resource.metadata.add_annotation("helm.sh/hook-weight", str(weight))
--- ca_cdk8s_constructs/helpers.py
+++ ca_cdk8s_constructs/helpers.py
@@ -1,7 +1,15 @@
 """A collection of helper functions for cdk8s applications."""
 
-from cdk8s import ApiObjectMetadata, ApiObjectMetadataDefinition
+from typing import Protocol
+
+import cdk8s
 from constructs import Construct
+
+
+class HasApiObjectMetadata(Protocol):
+    """Protocol for objects that have an ApiObjectMetadataDefinition property."""
+
+    metadata: cdk8s.ApiObjectMetadataDefinition
 
 
 def add_labels(construct: Construct, labels: dict[str, str]) -> None:
@@ -17,8 +25,28 @@ def add_labels(construct: Construct, labels: dict[str, str]) -> None:
 
     for resource in construct.node.children:
         if hasattr(resource, "metadata") and isinstance(
-            resource.metadata, (ApiObjectMetadataDefinition, ApiObjectMetadata)
+            resource.metadata, cdk8s.ApiObjectMetadataDefinition
         ):
             for key, value in labels.items():
                 resource.metadata.add_label(key, value)
         add_labels(resource, labels)
+
+
+def add_annotations(construct: Construct, annotations: dict[str, str]) -> None:
+    """
+    Recursively add the supplied labels to all resources in
+    the construct and all its children that have a metadata
+    property.
+
+    args:
+        construct: The construct to add labels to recursively.
+        annotations: The annotations to add to the construct.
+    """
+
+    for resource in construct.node.children:
+        if hasattr(resource, "metadata") and isinstance(
+            resource.metadata, cdk8s.ApiObjectMetadataDefinition
+        ):
+            for key, value in annotations.items():
+                resource.metadata.add_annotation(key, value)
+        add_annotations(resource, annotations)
